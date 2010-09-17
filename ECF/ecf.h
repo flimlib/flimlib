5d517d3b3218bdcffb43aaa8d3eeff8b91afc47f
Curtis Rueden <ctrueden@wisc.edu>
2010-09-17 22:28:01 +0000

    Add SLIM-curve project.

---
+++ ECF/ecf.h
@@ -0,0 +1,228 @@
+/*
+Gray Institute's Exponential Curve Fitting (ECF) Library
+Copyright (C) 2003-@year@ Gray Institute for Radiation Oncology and Biology 
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/* This is Ecf.h, the public header file for the  ECF library. */
+
+#ifndef _GCI_ECF
+#define _GCI_ECF
+
+/* #defines which are publically needed */
+
+typedef enum { NOISE_CONST, NOISE_GIVEN, NOISE_POISSON_DATA,
+	       NOISE_POISSON_FIT, NOISE_GAUSSIAN_FIT, NOISE_MLE } noise_type;
+
+typedef enum { FIT_GLOBAL_MULTIEXP, FIT_GLOBAL_STRETCHEDEXP } fit_type;
+
+typedef enum { ECF_RESTRAIN_DEFAULT, ECF_RESTRAIN_USER } restrain_type;
+
+/* Single transient analysis functions */
+
+// the next fn uses GCI_triple_integral_*() to fit repeatedly until chisq_target is met
+int GCI_triple_integral_fitting_engine(float xincr, float y[], int fit_start, int fit_end,
+							  float instr[], int ninstr, noise_type noise, float sig[],
+							  float *Z, float *A, float *tau, float *fitted, float *residuals,
+							  float *chisq, float chisq_target);
+// the next fn uses GCI_marquardt_instr() to fit repeatedly until chisq_target is met
+int GCI_marquardt_fitting_engine(float xincr, float *trans, int ndata, int fit_start, int fit_end, 
+						float prompt[], int nprompt,
+						noise_type noise, float sig[],
+						float param[], int paramfree[],
+					   int nparam, restrain_type restrain,
+					   void (*fitfunc)(float, float [], float *, float [], int),
+					   float *fitted, float *residuals, float *chisq,
+					   float **covar, float **alpha, float **erraxes,
+					   float chisq_target, int chisq_percent);
+
+int GCI_triple_integral(float xincr, float y[],
+						int fit_start, int fit_end,
+						noise_type noise, float sig[],
+						float *Z, float *A, float *tau,
+						float *fitted, float *residuals,
+						float *chisq, int division);
+int GCI_triple_integral_instr(float xincr, float y[],
+							  int fit_start, int fit_end,
+							  float instr[], int ninstr,
+							  noise_type noise, float sig[],
+							  float *Z, float *A, float *tau,
+							  float *fitted, float *residuals,
+							  float *chisq, int division);
+
+int GCI_marquardt(float x[], float y[], int ndata,
+				  noise_type noise, float sig[],
+				  float param[], int paramfree[], int nparam,
+				  restrain_type restrain,
+				  void (*fitfunc)(float, float [], float *, float [], int),
+				  float *fitted, float *residuals,
+				  float **covar, float **alpha, float *chisq,
+				  float chisq_percent, float **erraxes);
+int GCI_marquardt_instr(float xincr, float y[],
+					int ndata, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[],
+					float param[], int paramfree[], int nparam,
+					restrain_type restrain,
+					void (*fitfunc)(float, float [], float *, float [], int),
+					float *fitted, float *residuals,
+					float **covar, float **alpha, float *chisq,
+					float chisq_percent, float **erraxes);
+void GCI_marquardt_cleanup(void);
+
+/* Global analysis analysis functions */
+
+int GCI_marquardt_global_exps_instr(float xincr, float **trans,
+					int ndata, int ntrans, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[], int ftype,
+					float **param, int paramfree[], int nparam,
+					restrain_type restrain,
+					float **fitted, float **residuals,
+					float chisq_trans[], float *chisq_global, int *df,
+					int drop_bad_transients);
+int GCI_marquardt_global_generic_instr(float xincr, float **trans,
+					int ndata, int ntrans, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[],
+					float **param, int paramfree[], int nparam, int gparam[],
+					restrain_type restrain,
+					void (*fitfunc)(float, float [], float *, float [], int),
+					float **fitted, float **residuals,
+					float chisq_trans[], float *chisq_global, int *df);
+
+/* Support plane analysis functions */
+
+int GCI_SPA_1D_marquardt(
+				float x[], float y[], int ndata,
+				noise_type noise, float sig[],
+				float param[], int paramfree[], int nparam,
+				restrain_type restrain,
+				void (*fitfunc)(float, float [], float *, float [], int),
+				int spa_param, int spa_nvalues,
+				float spa_low, float spa_high,
+				float chisq[], void (*progressfunc)(float));
+int GCI_SPA_2D_marquardt(
+				float x[], float y[], int ndata,
+				noise_type noise, float sig[],
+				float param[], int paramfree[], int nparam,
+				restrain_type restrain,
+				void (*fitfunc)(float, float [], float *, float [], int),
+				int spa_param1, int spa_nvalues1,
+				float spa_low1, float spa_high1,
+				int spa_param2, int spa_nvalues2,
+				float spa_low2, float spa_high2,
+				float **chisq, void (*progressfunc)(float));
+int GCI_SPA_1D_marquardt_instr(
+				float xincr, float y[],
+				int ndata, int fit_start, int fit_end,
+				float instr[], int ninstr,
+				noise_type noise, float sig[],
+				float param[], int paramfree[], int nparam,
+				restrain_type restrain,
+				void (*fitfunc)(float, float [], float *, float [], int),
+				int spa_param, int spa_nvalues,
+				float spa_low, float spa_high,
+				float chisq[], float chisq_target, void (*progressfunc)(float));
+int GCI_SPA_2D_marquardt_instr(
+				float xincr, float y[],
+				int ndata, int fit_start, int fit_end,
+				float instr[], int ninstr,
+				noise_type noise, float sig[],
+				float param[], int paramfree[], int nparam,
+				restrain_type restrain,
+				void (*fitfunc)(float, float [], float *, float [], int),
+				int spa_param1, int spa_nvalues1,
+				float spa_low1, float spa_high1,
+				int spa_param2, int spa_nvalues2,
+				float spa_low2, float spa_high2,
+				float **chisq, float chisq_target, void (*progressfunc)(float));
+int GCI_SPA_1D_marquardt_global_exps_instr(
+					float xincr, float **trans,
+					int ndata, int ntrans, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[], int ftype,
+					float **param, int paramfree[], int nparam,
+					restrain_type restrain, int drop_bad_transients,
+					int spa_param, int spa_nvalues,
+					float spa_low, float spa_high,
+					float chisq_global[], int df[], void (*progressfunc)(float));
+int GCI_SPA_2D_marquardt_global_exps_instr(
+					float xincr, float **trans,
+					int ndata, int ntrans, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[], int ftype,
+					float **param, int paramfree[], int nparam,
+					restrain_type restrain, int drop_bad_transients,
+					int spa_param1, int spa_nvalues1,
+					float spa_low1, float spa_high1,
+					int spa_param2, int spa_nvalues2,
+					float spa_low2, float spa_high2,
+					float **chisq_global, int **df, void (*progressfunc)(float));
+int GCI_SPA_1D_marquardt_global_generic_instr(
+					float xincr, float **trans,
+					int ndata, int ntrans, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[],
+					float **param, int paramfree[], int nparam, int gparam[],
+					restrain_type restrain,
+					void (*fitfunc)(float, float [], float *, float [], int),
+					int spa_param, int spa_nvalues,
+					float spa_low, float spa_high,
+					float chisq_global[], int df[], void (*progressfunc)(float));
+int GCI_SPA_2D_marquardt_global_generic_instr(
+					float xincr, float **trans,
+					int ndata, int ntrans, int fit_start, int fit_end,
+					float instr[], int ninstr,
+					noise_type noise, float sig[],
+					float **param, int paramfree[], int nparam, int gparam[],
+					restrain_type restrain,
+					void (*fitfunc)(float, float [], float *, float [], int),
+					int spa_param1, int spa_nvalues1,
+					float spa_low1, float spa_high1,
+					int spa_param2, int spa_nvalues2,
+					float spa_low2, float spa_high2,
+					float **chisq_global, int **df, void (*progressfunc)(float));
+
+/* Setting restraints */
+
+int GCI_set_restrain_limits(int nparam, int restrain[],
+							float minval[], float maxval[]);
+
+/* Predefined fitting models */
+
+void GCI_multiexp_lambda(float x, float param[],
+			 float *y, float dy_dparam[], int nparam);
+void GCI_multiexp_tau(float x, float param[],
+		      float *y, float dy_dparam[], int nparam);
+void GCI_stretchedexp(float x, float param[],
+		      float *y, float dy_dparam[], int nparam);
+
+/* Utility functions */
+float **GCI_ecf_matrix(long nrows, long ncols);
+void GCI_ecf_free_matrix(float **m);
+
+void ECF_ExportParams_start (char path[]);
+void ECF_ExportParams_stop (void);
+
+#endif /* _GCI_ECF */
+
+// Emacs settings:
+// Local variables:
+// mode: c
+// c-basic-offset: 4
+// tab-width: 4
+// End:
