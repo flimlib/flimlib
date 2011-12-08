bc93beff8a54f9c14c1122dcd6521547c156adfb
Paul Barber <paul.barber@rob.ox.ac.uk>
2011-12-08 16:14:35 +0000

    Unnecessary define

--- src/main/c/GCI_Phasor.h
+++ src/main/c/GCI_Phasor.h
@@ -20,11 +20,6 @@ Copyright (c) 2010, 2011, Gray Institute University of Oxford & UW-Madison LOCI.
 #ifndef PHASOR_FITTING_H
 #define PHASOR_FITTING_H
 
-
-
-#define PHASOR_MONO_EXP_MODEL    1
-
-
 int    GCI_Phasor(float xincr, float y[], int fit_start, int fit_end,
 							  float *Z, float *u, float *v, float *taup, float *taum, float *tau, float *fitted, float *residuals,
 							  float *chisq);
