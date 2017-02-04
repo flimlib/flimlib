606d3f2ef90acaa468e72c5fab07aeaefafe8497
paulbarber <paul.barber@oncology.ox.ac.uk>
2017-02-04 06:18:08 +0000

    C++ interface basically works for RLD and LMA. There is tricky stuff with the start of the data, start of the fit and the length on the instrument response that is hard to simplify.

--- src/main/cpp/slim-curve.hpp
+++ src/main/cpp/slim-curve.hpp
@@ -23,6 +23,12 @@
 
  /* 
   C++ interface for SLIM Curve.
+
+  Caller _has_ to provide:
+	transient array
+	ndata
+	param array
+
 */
 
 #include <stdio.h>      /* printf, scanf, NULL */
@@ -33,43 +39,179 @@
 
 #define SLIM_CURVE_SUCCESS 0
 #define SLIM_CURVE_MEMORY_ERROR -1
-#define SLIM_CURVE_PARAMETER_ERROR -2
+#define SLIM_CURVE_SETTINGS_ERROR -2
 #define SLIM_CURVE_FITTING_ERROR -3
+
+#define SLIM_CURVE_MONO 0
+#define SLIM_CURVE_BI 1
+#define SLIM_CURVE_TRI 2
+#define SLIM_CURVE_STRETCHED 3
+
 
 // Set default values
 float chisq_target = 1.0f;
 
 class SLIMCurve {
+	// Local stores in case user does not provide them
+	int * _paramfree = NULL;
+	int _nparamfree = 0;
+	float *_fitted = NULL;
+	float *_residuals = NULL;
+	float _chisq = 0.0f;
+	float **_covar = NULL;
+	float **_alpha = NULL;
+	float **_err_axes = NULL;
+
 public:
 	float xincr;		///< The time increment inbetween the values in the y array.
 	float *transient;   ///< The transient (time resolved) signal to be analysed, the 'data'.
 	int ndata;          ///< The number of data points.
 	int fit_start;      ///< The index into the y array marking the start to the data to be used in the fit.
 	int fit_end;        ///< The index into the y array marking the end of the data to be used in the fit.
-	float *instr=NULL;  ///< The instrument reponse (IRF) or prompt signal to be used (optional, can be NULL).
-	int ninstr=0;       ///< The number of data points in the prompt (ignored if prompt = NULL).
-	noise_type noise = NOISE_POISSON_FIT;   ///< The #noise_type to be used.
+	float *instr;       ///< The instrument reponse (IRF) or prompt signal to be used (optional, can be NULL).
+	int ninstr;         ///< The number of data points in the prompt (ignored if prompt = NULL).
+	noise_type noise;   ///< The #noise_type to be used.
 	float *sig;         ///< The standard deviation at each data point in y if #noise_type NOISE_GIVEN is used (optional, can pass NULL).
 	float *param;       ///<
 	int *paramfree;     ///< An array indicating which parameters are free (1), fixed (0)
 	int nparam;         ///< The number of parameters.
 	restrain_type restrain; ///< Parameter #restrain_type.Normally use ECF_RESTRAIN_DEFAULT.Use ECF_RESTRAIN_USER if restraining parameters has been setup via GCI_set_restrain_limits.
 	float *fitted;      ///< An array containing values fitted to the data, the 'fit'. Fit points are coincident in time with the data points.
-	float *residuals;   ///<
-	float *chisq;       ///< The resulting raw chi squared value of the fit. To get the reduced chisq, divide by the degrees of freedom (fit_start - fit_end - nparam)
-	float chisq_target=1.0f; ///< A raw chi squared value to aim for. If this value is reached fitting will stop. If you want to aim for a reduced chisq (say 1.1 or 1.0) you must multiply by the degree of freedom. (TRI2: "Try refits")
-	float chisq_delta=0.000001f;  ///< An individual fit will continue if the chisq value changes by more then this amount.Try 1E-5. (TRI2: "Stopping Criterion")
-	int chisq_percent=95;  ///< Defines the confidence interval when calculating the error axes, e.g. 95 % .
+	float *residuals;   ///< An array containing the difference between the fit and the data. Points are coincident in time with the data points.
+	float *chisq;       ///< The resulting raw chi squared value of the fit. To get the reduced chisq, divide by the degrees of freedom (fit_end - fit_start - nparam), see getReducedChiSq()
+	float chisq_target; ///< A raw chi squared value to aim for. If this value is reached fitting will stop. If you want to aim for a reduced chisq (say 1.1 or 1.0) you must multiply by the degree of freedom. (TRI2: "Try refits")
+	float chisq_delta;  ///< An individual fit will continue if the chisq value changes by more then this amount.Try 1E-5. (TRI2: "Stopping Criterion")
+	int chisq_percent;  ///< Defines the confidence interval when calculating the error axes, e.g. 95 % .
 	float **covar;      ///< covar The covariance matrix. Allocate with a square matrix with #GCI_ecf_matrix(nparam, nparam).
 	float **alpha;      ///< The alpha matrix.Allocate with a square matrix with #GCI_ecf_matrix(nparam, nparam).
 	float **err_axes;   ///< The dimensions of the confidence ellipsoid of the chisq.See chisq_percent below.Allocate with a square matrix with #GCI_ecf_matrix(nparam, nparam).
 	int iterations;     ///< Store of the number of iterations performed.
 
-	void(*fitfunc)(float, float[], float *, float[], int);
+	void(*fitfunc)(float x, float param[], float *y, float dy_dparam[], int nparam);  ///< A function that executes the fitting function at pt x, given the parameters
 
-	SLIMCurve() {}
+	SLIMCurve() {
+		// Default values
+		xincr = 1.0;		
+		transient = NULL;
+		ndata = 0;          
+		fit_start = 0;      
+		fit_end = 0;        
+		instr = NULL;
+		ninstr = 0;         
+		noise = NOISE_POISSON_FIT;
+		sig = NULL;
+		param = NULL;
+		paramfree = NULL;
+		nparam = 0;         
+		restrain = ECF_RESTRAIN_DEFAULT;
+		fitted = NULL;  
+		residuals = NULL;
+		chisq = NULL;
+		chisq_target = 1.0;
+		chisq_delta = 0.0000001f;
+		chisq_percent = 95;  
+		covar = NULL;
+		alpha = NULL;
+		err_axes = NULL;   
+		iterations = 0; 
+		fitfunc = GCI_multiexp_tau;
+	}
 
-	~SLIMCurve(){}
+	~SLIMCurve(){
+		freePrivateVars();  // Should be done by fit functions, but just make sure.
+	}
+
+	int checkValues() {
+		if (transient == NULL) return SLIM_CURVE_SETTINGS_ERROR;
+		if (ndata == 0) return SLIM_CURVE_SETTINGS_ERROR;
+
+		if (fit_start < 0) fit_start = 0;
+		if (fit_end <= fit_start) fit_end = ndata-1;
+		if (fit_end > ndata-1) return SLIM_CURVE_SETTINGS_ERROR;
+
+		if (noise == NOISE_GIVEN && sig == NULL) return SLIM_CURVE_SETTINGS_ERROR;
+
+		if (paramfree == NULL) {
+			_paramfree = (int *)malloc(nparam * sizeof(int));
+			if (_paramfree == NULL) return SLIM_CURVE_MEMORY_ERROR;
+			for (int i = 0; i < nparam; i++) {
+				_paramfree[i] = 1;
+			}
+			paramfree = _paramfree;
+			_nparamfree = nparam;
+		}
+		else {
+			for (int i = 0, _nparamfree = 0; i < nparam; i++) {
+				if (paramfree[i]) _nparamfree++;
+			}
+		}
+
+		if (fitted == NULL) {
+			_fitted = (float *)malloc(ndata * sizeof(int));
+			if (_fitted == NULL) return SLIM_CURVE_MEMORY_ERROR;
+			fitted = _fitted;
+		}
+
+		if (residuals == NULL) {
+			_residuals = (float *)malloc(ndata * sizeof(int));
+			if (_residuals == NULL) return SLIM_CURVE_MEMORY_ERROR;
+			residuals = _residuals;
+		}
+
+		if (chisq == NULL) chisq = &_chisq;
+
+		if (covar == NULL) {
+			_covar = GCI_ecf_matrix(ndata, ndata);
+			if (_covar == NULL) return SLIM_CURVE_MEMORY_ERROR;
+			covar = _covar;
+		}
+
+		if (alpha == NULL) {
+			_alpha = GCI_ecf_matrix(ndata, ndata);
+			if (_alpha == NULL) return SLIM_CURVE_MEMORY_ERROR;
+			alpha = _alpha;
+		}
+
+		if (err_axes == NULL) {
+			_err_axes = GCI_ecf_matrix(ndata, ndata);
+			if (_err_axes == NULL) return SLIM_CURVE_MEMORY_ERROR;
+			err_axes = _err_axes;
+		}
+
+		return SLIM_CURVE_SUCCESS;
+	}
+
+	void freePrivateVars() {
+		if (_paramfree) {
+			free(_paramfree);
+			_paramfree = paramfree = NULL;
+		}
+
+		if (_fitted) {
+			free(_fitted);
+			_fitted = fitted = NULL;
+		}
+
+		if (_residuals) {
+			free(_residuals);
+			_residuals = residuals = NULL;
+		}
+
+		if (_covar) {
+			GCI_ecf_free_matrix(_covar);
+			_covar = covar = NULL;
+		}
+
+		if (_alpha) {
+			GCI_ecf_free_matrix(_alpha);
+			_alpha = alpha = NULL;
+		}
+
+		if (_err_axes) {
+			GCI_ecf_free_matrix(_err_axes);
+			_err_axes = err_axes = NULL;
+		}
+	}
 
 	int fitRLD() {
 
@@ -77,33 +219,56 @@ public:
 		float *A = &(param[1]);
 		float *tau = &(param[2]);
 
-		iterations = GCI_triple_integral_fitting_engine(xincr, transient, fit_start, fit_end,
-			instr, ninstr, noise, sig, Z, A, tau, fitted, residuals, chisq, chisq_target);
+		int err = checkValues();
+		if (err < 0) return err;
 
-		if (iterations >= 0) {
-			return SLIM_CURVE_SUCCESS;
-		}
-		else {
-			return SLIM_CURVE_FITTING_ERROR;
-		}
+		iterations = GCI_triple_integral_fitting_engine(xincr, &transient[fit_start], ninstr, fit_end - fit_start,
+			instr, ninstr, noise, sig, Z, A, tau, fitted, residuals, chisq, chisq_target*(fit_end - fit_start + ninstr - 3));
+
+		freePrivateVars();
+
+		return iterations;
 	}
 
+	int fitLMA(int type) {
 
+		switch (type)
+		{
+		case SLIM_CURVE_MONO:
+			nparam = 3;
+			fitfunc = GCI_multiexp_tau;
+			break;
+		case SLIM_CURVE_BI:
+			nparam = 5;
+			fitfunc = GCI_multiexp_tau;
+			break;
+		case SLIM_CURVE_TRI:
+			nparam = 7;
+			fitfunc = GCI_multiexp_tau;
+			break;
+		case SLIM_CURVE_STRETCHED:
+			fitfunc = GCI_stretchedexp;
+			nparam = 4;
+			break;
+		default:
+			break;
+		}
 
-	int fitLMA() {
+		int err = checkValues();
+		if (err < 0) return err;
 
-		iterations = GCI_marquardt_fitting_engine(xincr, transient, ndata, fit_start, fit_end,
+		iterations = GCI_marquardt_fitting_engine(xincr, &transient[fit_start], ndata - fit_start, ninstr, fit_end - fit_start,
 			instr, ninstr, noise, sig, param, paramfree, nparam, restrain, fitfunc,
-			fitted, residuals, chisq, covar, alpha, err_axes, chisq_target, chisq_delta, chisq_percent);
+			fitted, residuals, chisq, covar, alpha, err_axes, chisq_target*(fit_end - fit_start + ninstr - _nparamfree), chisq_delta, chisq_percent);
 
-		if (iterations >= 0) {
-			return SLIM_CURVE_SUCCESS;
-		}
-		else {
-			return SLIM_CURVE_FITTING_ERROR;
-		}
+		freePrivateVars();
+
+		return iterations;
 	}
 
-  
+	float getReducedChiSq(void) {
+		return (*chisq / (fit_end - fit_start - nparam));
+	}
+
 };
 
--- src/slim-curve-cmd/cpp/main.cpp
+++ src/slim-curve-cmd/cpp/main.cpp
@@ -25,47 +25,134 @@
 Main.cpp for the stand alone executable version of SLIM Curve via the C++ interface.
 
 */
+#include <iostream>
+using namespace std;
 
 #include "slim-curve.hpp"
 
 // Test Data
 float transient[] = {43, 39, 50, 46, 56, 63, 62, 74, 60, 72, 58, 47, 41, 69, 69, 58, 55, 37, 55, 50, 52, 59, 51, 52, 51, 50, 53, 40, 45, 34, 54, 44, 53, 47, 56, 62, 66, 82, 90, 108, 122, 323, 1155, 4072, 8278, 11919, 13152, 13071, 12654, 11946, 11299, 10859, 10618, 10045, 9576, 9208, 9113, 8631, 8455, 8143, 8102, 7672, 7384, 7463, 7254, 6980, 6910, 6411, 6355, 6083, 5894, 5880, 5735, 5528, 5343, 5224, 4933, 5026, 4914, 4845, 4681, 4426, 4485, 4271, 4295, 4183, 3989, 3904, 3854, 3801, 3600, 3595, 3434, 3457, 3291, 3280, 3178, 3132, 2976, 2973, 2940, 2770, 2969, 2851, 2702, 2677, 2460, 2536, 2528, 2347, 2382, 2380, 2234, 2251, 2208, 2115, 2136, 2000, 2006, 1970, 1985, 1886, 1898, 1884, 1744, 1751, 1797, 1702, 1637, 1547, 1526, 1570, 1602, 1557, 1521, 1417, 1391, 1332, 1334, 1290, 1336, 1297, 1176, 1189, 1220, 1209, 1217, 1140, 1079, 1059, 1074, 1061, 1013, 1075, 1021, 1012, 940, 982, 866, 881, 901, 883, 893, 845, 819, 831, 758, 794, 779, 772, 779, 791, 729, 732, 687, 690, 698, 661, 647, 668, 642, 619, 629, 656, 579, 579, 600, 563, 584, 531, 554, 526, 484, 530, 515, 493, 502, 479, 445, 439, 466, 431, 423, 451, 412, 415, 393, 404, 390, 398, 352, 394, 376, 338, 377, 367, 355, 352, 375, 339, 347, 316, 295, 322, 311, 294, 304, 264, 293, 294, 283, 278, 302, 253, 259, 252, 278, 254, 245, 246, 242, 226, 241, 222, 198, 197, 245, 221, 228, 224, 216, 174, 166, 163, 127, 122 };
 const int ndata = 256;
-int fit_start = 45;
-int fit_end = 200;
+int fit_start_rise = 38;  // near the first rise
+int fit_start_peak = 46;  // near the peak
+int fit_end = 255;
 float instr[] = { 0.00911042f, 0.0388225f, 0.131711f, 0.252389f, 0.27723f, 0.180233f, 0.0792762f, 0.0312277f };
 const int ninstr = 8;
 float xincr = 0.058628749f;
-
-// Test return values
-float params[MAXFIT] = { 0 };
-float fitted[ndata];
-float residuals[ndata];
-float chisq;
 
 /* Entry point
 */
 int main(int argc, const char * argv[])
 {
+	// Return values
+	float params[MAXFIT] = { 0 };
+	float fitted[ndata];
+	float residuals[ndata];
+	float chisq;
+	int iterations = 0;
+
 	// Create a fitter
 	SLIMCurve SlimCurve;
 
 	// Load up the fitter inputs
 	SlimCurve.transient = transient;
 	SlimCurve.ndata = ndata;
-	SlimCurve.fit_start = fit_start;
-	SlimCurve.fit_end = fit_end;
-	SlimCurve.instr = instr;
-	SlimCurve.ninstr = ninstr;
-	SlimCurve.xincr = xincr;
 	SlimCurve.param = params;   // starting values, will contain the fitted values at end of fit.
-
-	// Assign the fitter Outputs
-	SlimCurve.fitted = fitted;
-	SlimCurve.residuals = residuals;
+	SlimCurve.xincr = xincr;
 	SlimCurve.chisq = &chisq;
 
-	// Run the fit function
-	SlimCurve.fitRLD();
+	// Run the RLD fit function: Basic fit, no prompt, on data subset
+	SlimCurve.fit_start = fit_start_peak;
+	SlimCurve.fit_end = fit_end;
+	iterations = SlimCurve.fitRLD();
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitRLD\n";
+		exit(0);
+	}
+	cout << "RLD Z:" << params[0] << " A:" << params[1] << " tau:" << params[2] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:" << iterations << "\n";
+
+	// Run the LMA fit function: will use params[] as a starting point
+	iterations = SlimCurve.fitLMA(SLIM_CURVE_MONO);
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitLMA\n";
+		exit(0);
+	}
+	cout << "LMA Z:" << params[0] << " A:" << params[1] << " tau:" << params[2] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:" << iterations << "\n";
+
+
+
+	// Use an instrument function
+	SlimCurve.instr = instr;
+	SlimCurve.ninstr = ninstr;
+	SlimCurve.fit_start = fit_start_rise;
+
+	// Run the RLD fit function: simple
+	iterations = SlimCurve.fitRLD();
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitRLD\n";
+		exit(0);
+	}
+	cout << "RLDi Z:" << params[0] << " A:" << params[1] << " tau:" << params[2] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:" << iterations << "\n";
+
+	// Assign the optional fitter Outputs to make plots if we want
+//	SlimCurve.fitted = fitted;
+//	SlimCurve.residuals = residuals;
+
+	// Run the LMA fit function: with instr
+	iterations = SlimCurve.fitLMA(SLIM_CURVE_MONO);
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitLMA\n";
+		exit(0);
+	}
+	cout << "LMAi Z:" << params[0] << " A:" << params[1] << " tau:" << params[2] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:" << iterations << "\n";
+
+	// Setup a resonable start for tri-exp
+	params[1] /= 3.0;      // divide the amplitude A1
+	params[3] = params[1]; // let A2 = A1
+	params[4] = params[2] / 2.0f; // let tau2 = tau1 / 2
+	params[5] = params[1]; // let A3 = A1
+	params[6] = params[2] / 3.0f; // let tau3 = tau1 / 2
+								 // all other params from last fit
+
+	// Run the LMA Bi exp
+	// all starting params from last fit
+	iterations = SlimCurve.fitLMA(SLIM_CURVE_BI);
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitLMA\n";
+		exit(0);
+	}
+	cout << "LMAi Z:" << params[0] << " A1:" << params[1] << " tau1:" << params[2] << " A2:" << params[3] << " tau2:" << params[4] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:"<< iterations << "\n";
+
+	// Run the LMA Tri exp
+	iterations = SlimCurve.fitLMA(SLIM_CURVE_TRI);
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitLMA\n";
+		exit(0);
+	}
+	cout << "LMAi Z:" << params[0] << " A1:" << params[1] << " tau1:" << params[2] << " A2:" << params[3] << " tau2:" << params[4] << " A3:" << params[5] << " tau3:" << params[6] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:" << iterations << "\n";
+
+	// Setup a resonable start for stretched-exp
+	params[2] = 2.0;
+	params[3] = 1.5;
+	// all other params from last fit
+
+	// TODO Let's restrain Z>0 ...
+
+
+	// Run the LMA stretched exp
+	iterations = SlimCurve.fitLMA(SLIM_CURVE_STRETCHED);
+	if (iterations <= 0) {
+		// An error occurred
+		cout << "ERROR with SlimCurve.fitLMA\n";
+		exit(0);
+	}
+	cout << "LMAi Z:" << params[0] << " A1:" << params[1] << " tau1:" << params[2] << " h:" << params[3] << " chisq:" << SlimCurve.getReducedChiSq() << " iterations:" << iterations << "\n";
+
 
 }
