34361a03ab053212799f6fc3d001ead29ec2f2a7
Curtis Rueden <ctrueden@wisc.edu>
2018-02-16 15:21:55 -0600

    Travis: run the cram tests
    This is a first cut, which assumes cram might not be installed,
    and tries to install it using pip. But is pip available here?

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Install needed tools
+if which cram; then
+  CRAM=cram
+else
+  pip install --user cram
+  CRAM=/cram
+  CRAM=$HOME/.local/bin/cram
+fi
+
 # Prepare the build environment
 rm -rf build
 mkdir -p build
@@ -8,5 +17,8 @@ cmake ..
 
 # Build the code
 make
+
+# Run the unit tests
+"$CRAM" ../tests
 
 # TODO: Maven deploy artifacts
