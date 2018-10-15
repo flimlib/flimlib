2ba24633130bf8b269a816dea8ff94e6f60be507
Xanthorapedia <dgao24@wisc.edu>
2018-10-15 16:35:32 -0500

    .travis/buil.sh: manually depoly if skipped
    No longer uses checksum for checking artifact existence.
    Checks each of the desired files on remote host and deploy after exists:remote determines the GAV exists and should be skipped.

--- .travis/build.sh
+++ .travis/build.sh
@@ -53,5 +53,55 @@ fi
 
 ls -lR ./target/
 
+# Deploy artifacts
+# Lifted from https://github.com/imagej/imagej-launcher/blob/f14435e80acbe7c84d52695a4794afb570ab65c8/.travis/build.sh
+# Get GAV
+groupId="$(sed -n 's/^  <groupId>\(.*\)<\/groupId>$/\1/p' pom.xml)"
+groupIdForURL="$(echo $groupId | sed -e 's/\./\//g')"
+artifactId="$(sed -n 's/^ <artifactId>\(.*\)<\/artifactId>$/\1/p' pom.xml)"
+version="$(sed -n 's/^  <version>\(.*\)<\/version>$/\1/p' pom.xml)"
+
+artifactPaths="target/checkout/target/*.jar"
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+then
+  classifier="linux_64"
+else
+  classifier="osx_64"
+fi
+
+if [ "$TRAVIS_SECURE_ENV_VARS" = true \
+  -a "$TRAVIS_PULL_REQUEST" = false \
+  -a -f "target/checkout/release.properties" ]
+then
+  echo "== Deploying binaries =="
+
+  # Check if a release has been deployed for that version
+  folderStatus=$(curl -s -o /dev/null -I -w '%{http_code}' http://maven.imagej.net/content/repositories/releases/$groupIdForURL/$artifactId/$version/)
+  if [ "$folderStatus" != "200" ]
+  then
+    exit $exit_code
+  fi
+
+  for artifactPath in artifactPaths; do
+    artifactFileName="${artifactPath##*/}"
+    extension="${artifactPath##*.}"
+    # Check if the launcher for that version has already been deployed
+    fileStatus=$(curl -s -o /dev/null -I -w '%{http_code}' http://maven.imagej.net/content/repositories/releases/$groupIdForURL/$artifactId/$version/$artifactFileName)
+    if [ "$fileStatus" != "200" ]
+    then
+      mvn deploy:deploy-file\
+        -Dfile="$artifactPath"\
+        -DrepositoryId="imagej.releases"\
+        -Durl="dav:https://maven.imagej.net/content/repositories/releases"\
+        -DgeneratePom="false"\
+        -DgroupId="$groupId"\
+        -DartifactId="$artifactId"\
+        -Dversion="$version"\
+        -Dclassifier="$classifier"\
+        -Dpackaging="$extension"
+    fi
+  done
+fi
+
 exit $exit_code
-# TODO: Maven deploy artifacts
