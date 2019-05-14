7747ce6703b90a1310ae273fb0b4ebe9946f43e6
Xanthorapedia <dgao24@wisc.edu>
2019-05-13 22:22:27 -0500

    Travis: xcode9.3 + oraclejdk on osx

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-# Install needed tools
-if which cram; then
-  CRAM=cram
-else
-  # Need to install cram
-  if which pip; then
-    PIP=pip
-  elif which pip2; then
-    PIP=pip2
-  else
-    echo "No cram, no pip. Cannot continue."
-    exit 1
-  fi
-  "$PIP" install --user cram
-  if which cram; then
-    CRAM=cram
-  else
-    for dir in \
-      /usr/local/bin \
-      "$HOME/Library/Python/2.7/bin" \
-      "$HOME/.local/bin"
-    do
-      test -f "$dir/cram" && CRAM="$dir/cram"
-    done
-  fi
-  if [ -z "$CRAM" ]; then
-    echo "Cram purportedly installed, but cannot find it."
-    "$PIP" show -f cram
-    exit 2
-  fi
-fi
-
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh $encrypted_58cee4862e74_key $encrypted_58cee4862e74_iv
 
@@ -43,7 +11,7 @@ exit_code=$?
 if [ ! -f ./target/checkout/release.properties ]
 then
   # Run the unit tests
-  "$CRAM" ./tests
+  python -m cram ./tests
 
   exit_code=$((exit_code | $?))
 fi
