7b0dfc60547a431c076be3f7392bd51fbb14a8dd
Kevin Tan <kktangent@gmail.com>
2021-04-16 01:27:12 -0500

    moved common code to helper functions

--- src/main/python/flimlib/cfunc.py
+++ src/main/python/flimlib/cfunc.py
@@ -10,6 +10,58 @@ _GCI_triple_integral_fitting_engine = _flimlib.GCI_triple_integral_fitting_engin
 _noise_types = {'NOISE_CONST': 0, 'NOISE_GIVEN': 1, 'NOISE_POISSON_DATA': 2,
                 'NOISE_POISSON_FIT': 3, 'NOISE_GAUSSIAN_FIT': 4, 'NOISE_MLE': 5}  # 4 and 5 should be an error
 
+def _prep_sig(noise_type, sig):
+    """
+    Helper function to make sure noise_type is valid and sig is the correct type 
+    for the noise_type given
+    """
+    if not noise_type in _noise_types.keys():
+        raise ValueError(
+            "invalid noise type. The valid types are: ", _noise_types.keys)
+    elif noise_type == 'NOISE_GIVEN':
+        sig = np.asarray(sig, dtype=np.float32)
+        if sig.ndim != 1:
+            raise ValueError("sig must be 1 dimensional")
+        sig = np.ctypeslib.as_ctypes(sig)
+    elif noise_type == 'NOISE_CONST':
+        sig = float(np.asarray(sig))  # convert to float
+        sig = ctypes.c_float(sig)
+    elif sig is not None:
+        warnings.warn("Expected sig=None for noise type " +
+                      str(noise_type) + ". The given value of sig will be ignored")
+        return None
+    return sig
+
+def _prep_instr_ninstr(instr):
+    ninstr = 0
+    if instr is not None:
+        instr = np.asarray(instr, dtype=np.float32)
+        if instr.ndim != 1:
+            raise ValueError("instr must be 1 dimensional")
+        ninstr = instr.shape[0] # presumably shorter than photon_count
+        instr = np.ctypeslib.as_ctypes(instr)
+    return instr, ninstr
+
+def _prep_common_fit_params(photon_count, fit_start, fit_end):
+    photon_count = np.asarray(photon_count, dtype=np.float32)
+    if photon_count.ndim != 1:
+        raise ValueError("photon_count must be a 1 dimensional")
+    
+    if fit_end is None:
+        fit_end = photon_count.shape[0]-1
+
+    fit_length = fit_end-fit_start
+    fitted = np.empty(fit_length, dtype=np.float32)
+    residuals = np.empty(fit_length, dtype=np.float32)
+
+    photon_count = np.ctypeslib.as_ctypes(photon_count)
+    fit_start = ctypes.c_int(fit_start)
+    fit_end = ctypes.c_int(fit_end)
+    fitted = np.ctypeslib.as_ctypes(fitted)
+    residuals = np.ctypeslib.as_ctypes(residuals)
+
+    return photon_count, fit_start, fit_end, fitted, residuals
+
 # C header
 
 # int GCI_triple_integral_fitting_engine(float xincr, float y[], int fit_start, int fit_end,
@@ -32,63 +84,29 @@ def GCI_triple_integral_fitting_engine(period, photon_count, fit_start=0, fit_en
     """
     put documentation here
     """
-
-    photon_count = np.asarray(photon_count, dtype=np.float32)
-    if photon_count.ndim != 1:
-        raise ValueError("photon_count must be a 1 dimensional")
-
-    if fit_end is None:
-        fit_end = photon_count.shape[0]-1
-
-    ninstr = 0
-    if instr is not None:
-        instr = np.asarray(instr, dtype=np.float32)
-        if instr.ndim != 1:
-            raise ValueError("instr must be 1 dimensional")
-        ninstr = instr.shape[0]
-        # presumably shorter than photon_count
-        instr = np.ctypeslib.as_ctypes(instr)
-
-    if noise_type in _noise_types.keys():
-        if noise_type == 'NOISE_GAUSSIAN_FIT' or noise_type == 'NOISE_MLE':
-            raise ValueError(
-                "Noise types 'NOISE_GAUSSIAN_FIT' and 'NOISE_MLE' are currently unimplemented")
-    else:
-        raise ValueError(
-            "invalid noise type. The valid types are: ", _noise_types.keys)
-
-    if noise_type == 'NOISE_GIVEN':
-        sig = np.asarray(sig, dtype=np.float32)
-        if sig.ndim != 1:
-            raise ValueError("sig must be 1 dimensional")
-        sig = np.ctypeslib.as_ctypes(sig)
-    elif noise_type == 'NOISE_CONST':
-        sig = float(np.asarray(sig))  # convert to float
-        sig = ctypes.c_float(sig)
-    elif sig is not None:
-        warnings.warn("Expected sig=None for noise type " +
-                      str(noise_type) + ". The given value of sig will be ignored")
-        sig = None
-
-    # exclusive? the fitted and residuals had strange final values if included last index
-    samples = fit_end-fit_start
-
     period = ctypes.c_float(period)
-    photon_count = np.ctypeslib.as_ctypes(photon_count)
-    fit_start = ctypes.c_int(fit_start)
-    fit_end = ctypes.c_int(fit_end)
+
+    photon_count, fit_start, fit_end, fitted, residuals = _prep_common_fit_params(
+        photon_count, fit_start, fit_end)
+
+    instr, ninstr = _prep_instr_ninstr(instr)
+
+    # this lack of implementation is unique to triple integral
+    if noise_type == 'NOISE_GAUSSIAN_FIT' or noise_type == 'NOISE_MLE':
+        raise ValueError(
+            "Noise types 'NOISE_GAUSSIAN_FIT' and 'NOISE_MLE' are currently unimplemented")
+
+    sig = _prep_sig(noise_type, sig)
+
     chisq_target = ctypes.c_float(chisq_target)
 
     Z, A, tau, chisq = ctypes.c_float(), ctypes.c_float(
     ), ctypes.c_float(), ctypes.c_float()  # output values
 
-    fitted = np.empty(samples, dtype=np.float32)
-    residuals = np.empty(samples, dtype=np.float32)
-
     tries = _GCI_triple_integral_fitting_engine(
         period, photon_count, fit_start, fit_end, instr, ninstr, 
-        _noise_types[noise_type], sig, Z, A, tau, np.ctypeslib.as_ctypes(fitted), 
-        np.ctypeslib.as_ctypes(residuals), chisq, chisq_target)
+        _noise_types[noise_type], sig, Z, A, tau, fitted, 
+        residuals, chisq, chisq_target)
     if(output_fitted_and_residuals):
         return (tries, Z.value, A.value, tau.value, chisq.value, fitted, residuals)
 
