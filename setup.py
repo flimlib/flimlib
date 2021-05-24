fe1ecbac42a64ac6e4ab7b9417cef68174218743
Kevin Tan <kktangent@gmail.com>
2021-05-24 11:36:45 -0500

    data files should be put in the flimlib folder

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-import setuptools
+import distutils.command.build
+import distutils.spawn
 import glob
 import os
 import sys
-import distutils.command.build
-import distutils.spawn
+
+import setuptools
+import setuptools.command.build_py
+import setuptools.command.build_ext
 
 
 class build_mvn(distutils.cmd.Command):
@@ -35,14 +38,33 @@ class build_mvn(distutils.cmd.Command):
             distutils.spawn.spawn([maven])
         finally:
             os.chdir(old_dir)
+
     def should_run_mvn(self):
         return True
 
-distutils.command.build.build.sub_commands.append(('build_mvn',build_mvn.should_run_mvn))
+class build_ext(setuptools.command.build_ext.build_ext):
+    def run(self):
+        self.run_command('build_mvn')
+        return super().run()
+
+_flimlib_dummy = setuptools.Extension(
+    'flimlib._flimlib_dummy',
+    sources=[
+        'src/main/python/flimlib/flimlib_dummy.c',
+    ]
+)
+
+distutils.command.build.build.sub_commands.append(
+    ('build_mvn', build_mvn.should_run_mvn))
 
 setuptools.setup(
     install_requires=["numpy>=1.12.0"],
-    cmdclass={'build_mvn': build_mvn},
+    cmdclass={'build_mvn': build_mvn, 'build_ext': build_ext},
     # Empty string means target directory
-    data_files=[(os.path.join('lib', 'site-packages', 'flimlib'), [os.path.join('target', 'natives', 'flimlib.dll')])],
-    )
+    #data_files=[(os.path.join('lib', 'site-packages', 'flimlib'),
+    #             [os.path.join('target', 'natives', 'flimlib.dll')])],
+    data_files=[('flimlib',
+                 [os.path.join('target', 'natives', 'flimlib.dll')])],
+    include_package_data=True,
+    ext_modules=[_flimlib_dummy]
+)
