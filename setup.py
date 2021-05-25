3eff94c233301ffe8ae210f538f8af1f28866e5b
Mark A. Tsuchida <matsuchida@wisc.edu>
2021-05-25 09:43:58 -0500

    Make setup.py work on macOS

--- setup.py
+++ setup.py
@@ -3,6 +3,35 @@ import os.path
 import setuptools
 import sys
 
+from distutils.command.build_ext import build_ext
+from distutils.unixccompiler import UnixCCompiler
+
+# Workaround for setuptools/distutils limitations described in:
+# https://github.com/pypa/setuptools/issues/1192 and
+# https://github.com/pypa/setuptools/issues/1732
+# In our case, we need -std=c++14 (or c++11) for macOS clang++, but setuptools
+# uses 'clang $CFLAGS' for both C and C++ sources. Passing a C++ std to the C
+# compiler is an error, so we need a mechanism to pass a flag only when
+# compiling C++ sources. (Credit to https://stackoverflow.com/a/65865696 for
+# this idea.)
+
+unix_cxx_flags = '-std=c++14' # We could also read os.environ.get('CXXFLAGS')
+
+class UnixCCxxCompiler(UnixCCompiler):
+    def _compile(self, obj, src, ext, cc_args, extra_postargs, pp_opts):
+        is_cxx_source = os.path.splitext(src)[-1] in ('.cpp', '.cxx', '.cc')
+        my_cc_args = ([unix_cxx_flags] + cc_args if is_cxx_source else cc_args)
+        super()._compile(obj, src, ext, my_cc_args, extra_postargs, pp_opts)
+
+class build_ext_c_cxx(build_ext):
+    def build_extension(self, ext):
+        if self.compiler.compiler_type == 'unix':
+            compiler = UnixCCxxCompiler()
+            for attr, value in self.compiler.__dict__.items():
+                setattr(compiler, attr, value)
+            self.compiler = compiler
+        super().build_extension(ext)
+
 
 # normpath cleans up slashes on Windows
 flimlib_c_sources = [os.path.normpath(p) for p in
@@ -30,4 +59,7 @@ flimlib_ext = setuptools.Extension(
 setuptools.setup(
     install_requires=["numpy>=1.12.0"],
     ext_modules=[flimlib_ext],
+    cmdclass={
+        'build_ext': build_ext_c_cxx,
+    },
 )
