69b8a8caf5f628926c643e4c555a3d1c38ecdb25
Kevin Tan <kktangent@gmail.com>
2021-05-28 23:09:33 -0500

    switched to assertAlmostEqual to check equivalence

--- src/test/python/test_flimlib.py
+++ src/test/python/test_flimlib.py
@@ -13,18 +13,22 @@ photon_count32 = a_in * np.exp(-time/tau_in)
 photon_count64 = np.asarray(photon_count32,dtype=np.float64)
 photon_count2d = np.zeros([256,256])
 
-photon_count_linear = a_in + tau_in * time
+linear_const = 1
+photon_count_linear = linear_const * time
 
 class Test3Integral(unittest.TestCase):
     def test_output_margin(self):
         result = flimlib.cfunc.GCI_triple_integral_fitting_engine(
-            period, photon_count32, sig=0.5)
-        self.assertTrue(math.isclose(result.A, a_in, abs_tol=0.5))
-        self.assertTrue(math.isclose(result.tau, tau_in, abs_tol=0.5))
-        self.assertTrue(math.isclose(result.Z, 0.0, abs_tol=0.5))
+            period, photon_count32)
+        
+        # test to an integer precision since the triple integral fit isn't as rigorous
+        self.assertAlmostEqual(result.A, a_in, 0)
+        self.assertAlmostEqual(result.tau, tau_in, 0)
+        self.assertAlmostEqual(result.Z, 0.0, 0)
         self.assertTrue(result.tries>0)
-        self.assertTrue(math.isclose(result.fitted[0], 10, abs_tol=0.5))
-        self.assertTrue(math.isclose(result.residuals[0], 0.0, abs_tol=0.5))
+        self.assertAlmostEqual(result.fitted[0], a_in, 0)
+        self.assertAlmostEqual(result.residuals[0], 0.0, 0)
+        self.assertAlmostEqual(result.residuals[-1], 0.0, 0)
 
 
     def test_photon_count(self):
@@ -88,10 +92,12 @@ class Test3Integral(unittest.TestCase):
 
     def test_noise_poisson_data(self):
         flimlib.cfunc.GCI_triple_integral_fitting_engine(period, photon_count32, noise_type='NOISE_POISSON_DATA')
+        # this should print a warning
         flimlib.cfunc.GCI_triple_integral_fitting_engine(period, photon_count32, noise_type='NOISE_POISSON_DATA',sig=2)
 
     def test_noise_poisson_fit(self):
         flimlib.cfunc.GCI_triple_integral_fitting_engine(period, photon_count32, noise_type='NOISE_POISSON_FIT')
+        # this should print a warning
         flimlib.cfunc.GCI_triple_integral_fitting_engine(period, photon_count32, noise_type='NOISE_POISSON_FIT',sig=2)
     
     def test_chisq_target(self):
@@ -102,12 +108,13 @@ class TestGCIPhasor(unittest.TestCase):
     def test_output_margin(self):
         result = flimlib.cfunc.GCI_Phasor(period, photon_count32)
         self.assertEqual(result.error_code, 0)
-        self.assertTrue(math.isclose(result.fitted[0], a_in, abs_tol=0.5))
-        self.assertTrue(math.isclose(result.residuals[0], 0.0, abs_tol=0.1))
+        self.assertAlmostEqual(result.fitted[0], a_in, 1)
+        self.assertAlmostEqual(result.residuals[0], 0.0, 1)
 
 
 def dummy_exp_tau(x, param):
-    y = param[0] + param[1]*np.exp(-x / param[2])
+    y = param[1]*np.exp(-x / param[2])
+    #y = param[0] + param[1]*np.exp(-x / param[2])
     dy_dparam = [1,0,0]
     dy_dparam[1] = np.exp(-x/param[2])
     dy_dparam[2] = param[1]*x*np.exp(-x/param[2])/(param[2]**2)
@@ -118,7 +125,8 @@ def dummy_exp_tau_predicate(n_param):
 
 # linear fit!
 def dummy_linear(x, param):
-    y = param[0] + param [1]*x
+    y = param[1]*x
+    #y = param[0] + param[1]*x
     dy_dparam = [1,0]
     dy_dparam[1] = x
     return y, dy_dparam
@@ -130,16 +138,16 @@ class TestGCIMarquardt(unittest.TestCase):
     def test_output_margin(self):
         param_in = [0,a_in+1,tau_in+1] # slight offset to detect if the fitting works!
         result = flimlib.cfunc.GCI_marquardt_fitting_engine(period, photon_count32, param_in)
-        
-        self.assertTrue(math.isclose(a_in,result.param[1],abs_tol=0.5))
-        self.assertTrue(math.isclose(tau_in,result.param[2],abs_tol=0.5))
+        self.assertAlmostEqual(0.0,result.param[0],1)
+        self.assertAlmostEqual(a_in,result.param[1],1)
+        self.assertAlmostEqual(tau_in,result.param[2],1)
 
     def test_paramfree(self):
         param_in = [0,a_in+1,tau_in+1] # slight offset to detect if the fitting works!
         result = flimlib.cfunc.GCI_marquardt_fitting_engine(period, photon_count32, 
             param_in, fitfunc=flimlib.cfunc.GCI_multiexp_tau_wrapped, paramfree=[1,0,1])
         # second parameter should have been held fixed!
-        self.assertTrue(math.isclose(a_in+1, result.param[1],abs_tol=0.5))
+        self.assertAlmostEqual(a_in+1, result.param[1],1)
 
     def test_restraintype(self):
         with self.assertRaises(ValueError):
@@ -156,23 +164,23 @@ class TestGCIMarquardt(unittest.TestCase):
         param_in = [0,a_in+1,lambda_in+1] # slight offset to detect if the fitting works!
         result = flimlib.cfunc.GCI_marquardt_fitting_engine(period, photon_count32, param_in, fitfunc=flimlib.cfunc.GCI_multiexp_lambda)
 
-        self.assertTrue(math.isclose(a_in, result.param[1], abs_tol=0.5))
-        self.assertTrue(math.isclose(lambda_in, result.param[2],abs_tol=0.5))
+        self.assertAlmostEqual(a_in, result.param[1], 1)
+        self.assertAlmostEqual(lambda_in, result.param[2],1)
     
     def test_user_defined_fitfunc(self):
-        param_in = [0,a_in+1,tau_in+1] # slight offset to detect if the fitting works!
+        param_in = [0.0,a_in+1,tau_in+1] # slight offset to detect if the fitting works!
         fitfunc_in = flimlib.cfunc.FitFunc(dummy_exp_tau, nparam_predicate=dummy_exp_tau_predicate)
         result = flimlib.cfunc.GCI_marquardt_fitting_engine(period, photon_count32, param_in, fitfunc=fitfunc_in)
-        self.assertTrue(math.isclose(0.0,result.param[0],abs_tol=0.5))
-        self.assertTrue(math.isclose(a_in,result.param[1],abs_tol=0.5))
-        self.assertTrue(math.isclose(tau_in,result.param[2],abs_tol=0.5))
+        self.assertAlmostEqual(0.0,result.param[0],1)
+        self.assertAlmostEqual(a_in,result.param[1],1)
+        self.assertAlmostEqual(tau_in,result.param[2],1)
 
-        # linear fit!
-        param_in = [a_in+1, tau_in+1]
+        # linear fit! (did not work with the first param being nonzero)
+        param_in = [0, linear_const+1]
         fitfunc_in = flimlib.cfunc.FitFunc(dummy_linear, nparam_predicate=dummy_linear_predicate)
         result = flimlib.cfunc.GCI_marquardt_fitting_engine(period, photon_count_linear, param_in, fitfunc=fitfunc_in, noise_type='NOISE_CONST',sig=1.0)
-        self.assertTrue(math.isclose(a_in,result.param[0],abs_tol=0.5))
-        self.assertTrue(math.isclose(tau_in,result.param[1],abs_tol=0.5))
+        self.assertAlmostEqual(0.0,result.param[0],1)
+        self.assertAlmostEqual(linear_const,result.param[1],1)
 
     def test_nparam(self):
         with self.assertRaises(TypeError):
