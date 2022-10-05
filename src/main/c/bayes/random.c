f7c8f6c30accc2c2c33460f5c200a58854d0d392
Mark A. Tsuchida <matsuchida@wisc.edu>
2022-10-05 14:43:34 -0500

    Update/add license headers
    Manually selected subset of changes proposed by scijava-scripts
    release-version.sh. (Excluded changes to vendored source.)

--- src/main/c/bayes/random.c
+++ src/main/c/bayes/random.c
@@ -1,3 +1,25 @@
+/*-
+ * #%L
+ * FLIMLib package for exponential curve fitting of fluorescence lifetime data.
+ * %%
+ * Copyright (C) 2010 - 2022 University of Oxford and Board of Regents of the
+ * University of Wisconsin-Madison.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 
 // CVI Random function seems to be better than the rand1 and rand2 functions that were used
 // according to the dieharder tests: http://www.phy.duke.edu/~rgb/General/dieharder.php
